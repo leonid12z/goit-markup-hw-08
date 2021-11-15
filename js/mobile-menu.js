85257889b7a2b02080d1681f8bf4efdd7ba049ed
leonid12z <lelik_ceh@ukr.net>
2021-11-15 19:09:45 +0200

    15.11.2021.+08+d/z

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -20,6 +20,7 @@
 
   // Закрываем мобильное меню на более широких экранах
   // в случае изменения ориентации устройства.
+  
   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
     if (!e.matches) return;
     mobileMenu.classList.remove('is-open');
